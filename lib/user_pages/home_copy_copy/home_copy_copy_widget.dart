79cb35411846ca6f7eceeb54372b20cfa0cdc15c
supershadoe <supershadoe@proton.me>
2024-06-04 18:01:00 +0530

    Change initial page

--- lib/user_pages/home_copy_copy/home_copy_copy_widget.dart
+++ lib/user_pages/home_copy_copy/home_copy_copy_widget.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 
 import '/auth/firebase_auth/auth_util.dart';
@@ -14,18 +13,15 @@ import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
-import 'dart:math';
 import '/custom_code/actions/index.dart' as actions;
 import '/custom_code/widgets/index.dart' as custom_widgets;
 import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:octo_image/octo_image.dart';
 import 'package:provider/provider.dart';
 
@@ -47,7 +43,7 @@ class _HomeCopyCopyWidgetState extends State<HomeCopyCopyWidget>
 
   final animationsMap = <String, AnimationInfo>{};
 
-  var showPermsRequest = true;
+  var showPermsRequest = false;
 
   @override
   void initState() {
