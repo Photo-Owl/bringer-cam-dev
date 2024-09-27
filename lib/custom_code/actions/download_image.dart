46188fc84413f6341fd55ca1e2cbfe5fdb5c1978
Claudin <69135470+ClaudinNarsis@users.noreply.github.com>
2024-09-27 22:55:15 +0530

    liveness changes

--- lib/custom_code/actions/download_image.dart
+++ lib/custom_code/actions/download_image.dart
@@ -1,4 +1,8 @@
 // Automatic FlutterFlow imports
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
 import '/backend/backend.dart';
 import '/backend/schema/structs/index.dart';
 import '/backend/schema/enums/enums.dart';
@@ -8,6 +12,7 @@ import '/flutter_flow/flutter_flow_util.dart';
 import 'index.dart'; // Imports other custom actions
 import '/flutter_flow/custom_functions.dart'; // Imports custom functions
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 // Begin custom action code
 // DO NOT REMOVE OR MODIFY THE CODE ABOVE!
 
@@ -24,7 +29,20 @@ Future<void> downloadImage(String url, String key) async {
     );
     return;
   }
+  if (Platform.isIOS) {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$fileName';
 
+    // Download the file
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      // Save the file
+      File file = File(filePath);
+      await file.writeAsBytes(response.bodyBytes);
+      print('File downloaded to: $filePath');
+    } else {
+      print('Failed to download file: ${response.statusCode}');
+  }
   AndroidDownloadManager.enqueue(
     downloadUrl: url,
     downloadPath: '/sdcard/Pictures/Social Gallery/',
